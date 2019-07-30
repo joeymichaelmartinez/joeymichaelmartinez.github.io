a622dba8cf15d8534e4036dd3c4ce7208bdaa58c
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2019-07-29 22:17:59 -0700

    moving readme for react into documentation folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 
 let test = <h2> The </h2>;
 
